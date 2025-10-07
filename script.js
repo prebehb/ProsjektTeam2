9a8b145db9af11e563fddffe776fdbf6c328e24e
ianulrik <ianulrik03@gmail.com>
2025-10-07 15:51:43 +0200

    laget en et obkjekt som en konstant

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-alert('Hello World')
-alert('Hello Preben')
-alert('Hello Ian')
+const spiller = {
+    X : 100,
+    y: 300,
+    width: 40,
+    height: 40,
+    color: #0099ff,
+    vy: 0,
+    gravity: 1,
+    jumpPower: - 14,
+    isOnGround: true,
+};
+const bakke = 80; // høyde på bakken inne i spillet
 
-alert('Hello Eilif')
+function spill(){ //Selve spillet
+    if (spiller.y + spiller.height >= canvas.height - bakke){
+        
+    }
 
-
-
-
-alert("testing")
+}
